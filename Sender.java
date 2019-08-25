b0eb24234a3bd4733b65f61c6e63b5e7d9df25c7
NinjaEater <ninjaeater@iMac-de-NinjaEater.local>
2019-08-25 17:53:45 +0100

    Synchronization between threads

--- Sender.java
+++ Sender.java
@@ -1,7 +1,7 @@
 class Sender {
 
     // we could synchronized the whole method like this.
-    public synchronized void send(String msg) {
+    /*public synchronized void send(String msg) {
         System.out.println("Sending  " + msg);
         try {
             Thread.sleep(1000);
@@ -9,6 +9,23 @@ class Sender {
             System.out.println(e.getMessage());
         }
         System.out.println(msg + " Sent!");
+    }*/
+
+    // also we may have to synchronize only a part of the send method not all of it.
+    public void send(String msg) {
+        
+        synchronized(this) {
+            System.out.println("Sending  " + msg);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println(msg + " Sent!");
+        }
+        
     }
+
+
 
 }
